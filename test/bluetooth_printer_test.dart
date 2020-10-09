d3d732446bc77fcd7b7acb1eb1ce628419e72ca5
kaciya <kaciya@163.com>
2020-10-09 14:39:54 +0800

    去除版本变量

--- test/bluetooth_printer_test.dart
+++ test/bluetooth_printer_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:bluetooth_printer/bluetooth_printer.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('bluetooth_printer');
@@ -13,9 +12,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await BluetoothPrinter.platformVersion, '42');
   });
 }
